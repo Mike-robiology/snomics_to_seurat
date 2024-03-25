7b891871f4f5aca26f996266a502e9e3182d707b
Mike-robiology <michael.thomas21@imperial.ac.uk>
2024-03-25 15:54:05 +0000

    cleaned up old files, added further dependency

--- conf/bioc-requirements.r
+++
@@ -1,9 +0,0 @@
-bioc_pkgs<-c(
-    "EnsDb.Hsapiens.v86",
-    "BSgenome.Hsapiens.UCSC.hg38",
-    "GenomicRanges"
-)
-
-install.packages("BiocManager")
-requireNamespace("BiocManager")
-BiocManager::install(bioc_pkgs,ask=F)
